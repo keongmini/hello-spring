e018dd7e8afa756f2b654ab43d1c90518e7bdd7a
keongmini <kmnam09@gmail.com>
2023-01-23 15:03:08 +0900

    add spring bean 1. component scan and dependency injection

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,16 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller  // 컴포넌트 스캔
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired      // controller -> service 연결: dependency injection
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
