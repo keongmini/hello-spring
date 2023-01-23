872e2f7162454079940b97889f8fafeec2cf283c
keongmini <kmnam09@gmail.com>
2023-01-23 15:11:59 +0900

    add spring bean 2. add directly with code

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,7 +4,7 @@ import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-@Controller  // 컴포넌트 스캔
+@Controller  // 컴포넌트 스캔 - Autowired와 함께 해야함
 public class MemberController {
 
     private final MemberService memberService;
